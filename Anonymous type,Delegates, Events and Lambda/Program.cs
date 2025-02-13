950e6843d29de9d00aaf558b3b23c064e863e4ac
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-02-13 10:43:29 +0530

    Expression Tree trying

--- Anonymous type,Delegates, Events and Lambda/Program.cs
+++ Anonymous type,Delegates, Events and Lambda/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using Anonymous_type_Delegates__Events_and_Lambda;
 
 class Program
@@ -84,6 +85,20 @@ class Program
         List<int> numbers2 = new List<int> { 2, 5, 6, 8, 1, 3, 5, 5 };
         var count = numbers.Count(x => {  return x == 5; });
         Console.WriteLine(count);
+
+        //expresion tree example trying
+        Func<string,string,string>stringJoins = (str1,str2)=>string.Concat(str1,str2);
+        Expression<Func<string, string, string>> stringJoinExpr = (str1, str2) => string.Concat(str1, str2);
+
+
+        var func = stringJoinExpr.Compile();
+        var result = func("Hello"," World");
+        Console.WriteLine(result);
+
+        //OR
+
+        result = stringJoinExpr.Compile()("Hello","Everyone");
+        Console.WriteLine(result);
     }
 }
 

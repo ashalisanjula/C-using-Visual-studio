67817d9abcca90dd25b4aa949d8c9728534938d7
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-02-13 07:47:50 +0530

    Anonymous type part 2

--- Anonymous type,Delegates, Events and Lambda/Program.cs
+++ Anonymous type,Delegates, Events and Lambda/Program.cs
@@ -6,12 +6,25 @@
         {
             firstName = "King",
             lastName = "Kochhar",
-            salary = 12000
+            salary = 12000,
+            address = new {streetName = "Civil Lines", city="Delhi"},
+            project = new[]
+            {
+                new{projectName="Ecommerce",projectDuration="40 hours"},
+                new{projectName="Admin Portal",projectDuration="30 hours"},
+                new{projectName="Accounting",projectDuration="25 hours"}
+            }
         };
 
         Console.WriteLine(obj.firstName);
         Console.WriteLine(obj.lastName);
         Console.WriteLine(obj.salary);
+        Console.WriteLine(obj.address.streetName);
+        Console.WriteLine(obj.address.city);
+        foreach(var project in obj.project)
+        {
+            Console.WriteLine(project.projectName+ ":" +project.projectDuration);
+        }
     }
 }
 
--- Anonymous type,Delegates, Events and Lambda/obj/Debug/net8.0/Anonymous type,Delegates, Events and Lambda.AssemblyInfo.cs
+++ Anonymous type,Delegates, Events and Lambda/obj/Debug/net8.0/Anonymous type,Delegates, Events and Lambda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anonymous type,Delegates, Events and Lambda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa929a9c5b98e2d6a2cfb753fb05465f12b22f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab91429728867a28d9eab71b9cfd541ce6ebb48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anonymous type,Delegates, Events and Lambda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anonymous type,Delegates, Events and Lambda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

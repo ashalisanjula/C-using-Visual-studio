aab91429728867a28d9eab71b9cfd541ce6ebb48
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-02-13 07:32:19 +0530

    trying anonymous type part 1

---
+++ Anonymous type,Delegates, Events and Lambda/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var obj = new
+        {
+            firstName = "King",
+            lastName = "Kochhar",
+            salary = 12000
+        };
+
+        Console.WriteLine(obj.firstName);
+        Console.WriteLine(obj.lastName);
+        Console.WriteLine(obj.salary);
+    }
+}
+
